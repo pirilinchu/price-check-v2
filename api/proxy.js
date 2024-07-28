21732ea64b5db6e4502234ee7b6f6f1a82fcbf1b
Santiago Mendoza <santiagomendozaa6@gmail.com>
2024-07-28 12:11:36 -0400

    Try to fix json

--- api/proxy.js
+++ api/proxy.js
@@ -1,5 +1,3 @@
-// /api/proxy.js
-
 const fetch = require("node-fetch");
 const fs = require("fs");
 const path = require("path");
@@ -7,11 +5,32 @@ const path = require("path");
 const pricesFilePath = path.join(process.cwd(), "prices.json");
 
 const readPricesFromFile = () => {
-  return JSON.parse(fs.readFileSync(pricesFilePath, "utf-8"));
+  try {
+    if (fs.existsSync(pricesFilePath)) {
+      const data = fs.readFileSync(pricesFilePath, "utf-8");
+      return JSON.parse(data);
+    } else {
+      // Initialize the file if it does not exist
+      const initialData = { BUY: [], SELL: [], BUY_USD: [] };
+      fs.writeFileSync(
+        pricesFilePath,
+        JSON.stringify(initialData, null, 2),
+        "utf-8"
+      );
+      return initialData;
+    }
+  } catch (error) {
+    console.error("Error reading prices file:", error);
+    return { BUY: [], SELL: [], BUY_USD: [] };
+  }
 };
 
 const writePricesToFile = (data) => {
-  fs.writeFileSync(pricesFilePath, JSON.stringify(data, null, 2), "utf-8");
+  try {
+    fs.writeFileSync(pricesFilePath, JSON.stringify(data, null, 2), "utf-8");
+  } catch (error) {
+    console.error("Error writing prices file:", error);
+  }
 };
 
 export default async function handler(req, res) {
@@ -58,13 +77,17 @@ export default async function handler(req, res) {
     });
 
     const data = await apiResponse.json();
-    // const price = data.data[0].adv.price;
+    const price = data.data[0].adv.price;
 
-    // // Save the price to the JSON file
-    // const date = new Date().toISOString().split("T")[0]; // Current date
-    // const prices = readPricesFromFile();
-    // prices[tradeType].push({ date, price });
-    // writePricesToFile(prices);
+    // Save the price to the JSON file
+    const date = new Date().toISOString().split("T")[0]; // Current date
+    const prices = readPricesFromFile();
+    if (prices[tradeType]) {
+      prices[tradeType].push({ date, price });
+    } else {
+      prices[tradeType] = [{ date, price }];
+    }
+    writePricesToFile(prices);
 
     res.status(200).json(data);
   } catch (error) {
